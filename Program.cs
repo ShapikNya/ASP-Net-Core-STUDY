0bcb9d8f7328eb3555b3459670325da38ba3129c
ShapikNya <Shapik_psn@mail.ru>
2025-09-07 22:49:58 +0300

    Creating POST GET methods for API

--- Program.cs
+++ Program.cs
@@ -6,7 +6,97 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseWelcomePage();
+var todos = new List<TodoItem>() {
+    new TodoItem { Id = Guid.NewGuid(), Title ="homeWork", IsCompleted = false },
+    new TodoItem{Id = Guid.NewGuid(), Title = "Gym", IsCompleted = true},
+    new TodoItem{Id = Guid.NewGuid(), Title = "Clean room", IsCompleted = false}
+};
+
+
+app.Run(async (context) =>
+{
+    var request = context.Request;
+    var response = context.Response; response.ContentType = "text/html; charset=utf-8";
+
+    switch (request.Method)
+    {
+        case "GET":
+            {
+                if (request.Path != "/api/todos") break;
+
+                //Поиск по индексу
+                if (request.Query.ContainsKey("index"))
+                {
+                    try
+                    {
+                        int index = Convert.ToInt32(request.Query["index"]);
+                        var person = todos.ElementAtOrDefault(index);
+                        if (person != null)
+                        {
+                            string personJson = JsonSerializer.Serialize(person, new JsonSerializerOptions
+                            {
+                                WriteIndented = true
+                            });
+                            response.StatusCode = 201;
+                            await response.WriteAsync(personJson);
+                        }
+                        response.StatusCode = 400;
+                        await response.WriteAsync("Person с таким индексом не существует");
+                        break;
+                    }
+                    catch
+                    {
+                        response.StatusCode = 400;
+                        await response.WriteAsync("Неверная запись значения ключа");
+                        break;
+                    }
+                }
+                //Вывод всех заданий
+                string Json = JsonSerializer.Serialize(todos, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+                await response.WriteAsync(Json);
+                break;
+            }
+
+        case "POST":
+            {
+                if (request.Path != "/api/todos") break;
+
+                try
+                {
+                    var item = await request.ReadFromJsonAsync<TodoItem>();
+                    if (item == null) return;
+                    todos.Add(item);
+                    response.StatusCode = 201;
+                    await response.WriteAsync($"Задание {item.Title} успешно добавлено!");
+                }
+                catch
+                {
+                    response.StatusCode = 400;
+                    await response.WriteAsync("Неверная запись Json");
+                }
+
+                break;
+            }
+        default:
+            {
+                response.StatusCode = 500;
+                response.WriteAsync("Тип данного запроса не поддерживается"); break;
+            }
+    }
+
+
+});
 
 app.Run();
+
+public class TodoItem
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
 
--- obj/Debug/net8.0/ASP_study.AssemblyInfo.cs
+++ obj/Debug/net8.0/ASP_study.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP_study")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6967c7dbd18c4d2c10fcdb8b54039d907553d21f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP_study")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP_study")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
